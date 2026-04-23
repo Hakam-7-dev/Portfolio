c6914161aadcc1b2714ed039b76dbf0b8cdba649
Hakam-7-dev <hakam.us.hammoud@gmail.com>
2026-04-23 11:43:21 +0400

    removed feedback section

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
         <About />
         <Grid />
         <RecentProjects />
-        <Clients />
+        {/* <Clients /> */}
         <Experience />
         <Approach />
         <Footer />
